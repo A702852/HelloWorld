980413bf3b120a37531e90c9ff16b928d3b2a0eb
A702852 <43565812+A702852@users.noreply.github.com>
2019-05-15 17:14:16 +0530

    Select Best Alternative. (#2)
    * Select Best Alternative.
    
    * Removed obvious comments.

--- HelloWorldDemo/Program.cs
+++ HelloWorldDemo/Program.cs
@@ -9,22 +9,7 @@ namespace HelloWorldDemo
         static readonly string textFile = @"C:\Users\a702852\OneDrive - Atos\Documents\GitHub\HelloWorld\Test.txt";  
         
         static void Main(string[] args)
-        {
-            if (File.Exists(textFile))
-            {  
-                // Read entire text file content in one string  
-                string text = File.ReadAllText(textFile);  
-                Console.WriteLine(text);  
-            }  
-        
-            if (File.Exists(textFile))
-            {
-                // Read a text file line by line.
-                string[] lines = File.ReadAllLines(textFile);
-                foreach (string line in lines)
-                Console.WriteLine(line);
-            }
-        
+        {                     
             if (File.Exists(textFile))
             {
                 // Read file using StreamReader. Reads file line by line
